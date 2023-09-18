dc85cf36b4e3815f2b209e24f3133161f10534b1
edgarlandal <mlanda@uabc.edu.mx>
2023-09-17 21:27:19 -0700

    First Commit

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=256)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
